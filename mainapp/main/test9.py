66dd7992c800caed4f4d0dd6b69200bdcec95a49
n3trob3 <netrobeweb@gmail.com>
2021-08-24 12:08:20 +0100

    'update'

--- mainapp/main/test9.py
+++ mainapp/main/test9.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions('/usr/local/bin/chromedriver')
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
  
 # The place we will direct our WebDriver to
 url = 'http://www.srcmake.com/'
 
 # Creating the WebDriver object using the ChromeDriver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 
 # Directing the driver to the defined url
 driver.get(url)
