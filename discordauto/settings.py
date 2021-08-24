a2981adf05e8e27d4daf14924eee1c0d1a454c97
n3trob3 <netrobeweb@gmail.com>
2021-08-24 10:34:57 +0100

    'update'

--- discordauto/settings.py
+++ discordauto/settings.py
@@ -72,21 +72,21 @@ TEMPLATES = [
 # ASGI_APPLICATION = 'discordauto.asgi.application'
 
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [("127.0.0.1", 6379)],
-#         },
-#     },
-# }
-
-
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
+
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer"
+#     }
+# }
 
 
 
