a1ae626a2ff98af9d2254928b12c211147f45cd9
n3trob3 <netrobeweb@gmail.com>
2021-08-24 10:43:45 +0100

    'update'

--- discordauto/asgi.py
+++ discordauto/asgi.py
@@ -2,12 +2,14 @@
 import os
 from decouple import config
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import notification.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
